cc89ef40449fff1a96c53eb0411d0ca52fc0025f
Adam Page <adamfpage@gmail.com>
2021-01-05 01:11:03 -0800

    Add automatic slugified ID attributes to markdown heading content.

--- .eleventy.js
+++ .eleventy.js
@@ -4,6 +4,9 @@ const pluginPWA = require('eleventy-plugin-pwa');
 // Transforms
 const htmlMinTransform = require('./src/transforms/html-min-transform.js');
 
+const markdownIt = require("markdown-it");
+const markdownItAnchor = require("markdown-it-anchor");
+
 // Create a helpful production flag
 const isProduction = process.env.NODE_ENV === 'production';
 
@@ -11,6 +14,15 @@ module.exports = config => {
 
   // Pass images through to build folder.
   config.addPassthroughCopy('./src/assets/');
+
+  // Markdown overrides.
+  let markdownLibrary = markdownIt({
+    html: true,
+    xhtmlOut: true
+  }).use(markdownItAnchor, {
+    level: 1
+  });
+  config.setLibrary("md", markdownLibrary);
 
   // If in the production environment
   if (isProduction) {
